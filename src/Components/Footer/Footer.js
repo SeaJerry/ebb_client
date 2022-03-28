9bc7bf20eb03614b1e42cce50793c3e123200b6c
Jerry_Sea <Jsea@kindsys.us>
2022-03-28 11:23:01 -0700

    added footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -7,31 +7,33 @@ const Footer = () => {
   return (
     <div className="footer-container">
       <section className="footer-subscription">
-        <p className="footer-subscription-heading">Join the newsletter</p>
-        <p className="footer-subscription-text">Unsubscribe at any time</p>
-        <div className="input-areas">
-          <form>
-            <input
-              type="email"
-              name="email"
-              placeholder="Your Email"
-              className="footer-input"
-            />
+        <div className="footer-wrapper">
+          <p className="footer-subscription-heading">Join the newsletter</p>
+          <p className="footer-subscription-text">Unsubscribe at any time</p>
+          <div className="input-areas">
+            <form>
+              <input
+                type="email"
+                name="email"
+                placeholder="Your Email"
+                className="footer-input"
+              />
+            </form>
             <button className="footer-btn">Subscribe</button>
-          </form>
+          </div>
+        </div>
+        <div className="footer-icons-wrapper">
+          <a href="https://www.facebook.com/" className="icons">
+            <FaIcons.FaFacebook />
+          </a>
+          <a href="https://twitter.com/" className="icons">
+            <AiIcons.AiOutlineTwitter />
+          </a>
+          <a href="https://www.instagram.com/" className="icons">
+            <AiIcons.AiOutlineInstagram />
+          </a>
         </div>
       </section>
-      <div className="footer-icons-wrapper">
-        <a href="https://www.facebook.com/">
-          <FaIcons.FaFacebook />
-        </a>
-        <a href="https://twitter.com/">
-          <AiIcons.AiOutlineTwitter />
-        </a>
-        <a href="https://www.instagram.com/">
-          <AiIcons.AiOutlineInstagram />
-        </a>
-      </div>
     </div>
   );
 };
