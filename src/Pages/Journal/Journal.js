8d4aa867b9ddf4c124835abe2fbfbac1138b1aab
Jerry_Sea <Jsea@kindsys.us>
2022-03-29 16:53:17 -0700

    get request for blog posts functioning

--- src/Pages/Journal/Journal.js
+++ src/Pages/Journal/Journal.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React from "react";
+import "./Journal.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 
 const Journal = () => {
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // setLoading(true);
+      try {
+        const response = await axios.get(
+          "http://localhost:3000/posts/"
+        );
+
+        setData(response.data);
+        
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(data.length)
+  const listItem = data.map((item) => {
+
+    return (
+      <h1>{item.title}</h1>
+    )
+    
+    })
+
+
   return (
     <div>
-      
-    </div>
-  )
-}
+    <h2 className="title">Doing stuff with data</h2>
+    {listItem}
+  </div>
+  );
+};
 
-export default Journal
+export default Journal;
--- src/Pages/Write/Write.js
+++ src/Pages/Write/Write.js
@@ -1,26 +1,73 @@
 import React from "react";
 import "./Write.css";
+import { useState } from "react";
+import axios from "axios";
 
 const Write = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [username, setUsername] = useState("");
+  const [photo, setPhoto] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const newPost = {
+      username,
+      title,
+      description,
+      photo,
+    };
+
+    try {
+      const response = await axios.post("http://localhost:3000/posts", newPost);
+      response.data && window.location.replace("/#/Journal");
+      console.log(response);
+    } catch (error) {
+      console.log("Oops, Something Went Wrong");
+    }
+  };
+
   return (
     <section className="main-container">
       <div className="write-container">
-        <form className="write-form">
+        <form className="write-form" onSubmit={handleSubmit}>
           <div className="write-form-group">
-            <input className="write-inpt" type="text" placeholder="Image URL" />
+            <input
+              className="write-inpt"
+              type="text"
+              placeholder="Username"
+              onChange={(e) => setUsername(e.target.value)}
+            />
           </div>
           <div className="write-form-group">
-            <input className="write-inpt" type="text" placeholder="Title" />
+            <input
+              className="write-inpt"
+              type="text"
+              placeholder="Image URL"
+              onChange={(e) => setPhoto(e.target.value)}
+            />
+          </div>
+          <div className="write-form-group">
+            <input
+              className="write-inpt"
+              type="text"
+              placeholder="Title"
+              onChange={(e) => setTitle(e.target.value)}
+            />
           </div>
           <div className="content-container">
             <textarea
+              onChange={(e) => setDescription(e.target.value)}
               placeholder="Share Your Story"
+              type="text"
               id="content"
               cols="30"
               rows="10"
             ></textarea>
           </div>
-          <button className="submit-btn">Submit</button>
+          <button className="submit-btn" type="submit">
+            Submit
+          </button>
         </form>
       </div>
     </section>
